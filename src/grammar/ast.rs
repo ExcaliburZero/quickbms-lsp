32bc38814e15e9759a30d22b6bae98cfc6aa5539
Christopher Wells <cwellsny@nycap.rr.com>
2021-04-24 00:51:09 -0700

    Start implementing keyword finding

--- src/grammar/ast.rs
+++ src/grammar/ast.rs
@@ -6,7 +6,13 @@ pub struct File {
 
 impl File {
     fn get_hover_message(&self, location: LineColumn) -> Option<String> {
-        panic!()
+        for (loc_range, keyword) in self.keywords_by_location.iter() {
+            if loc_range.contains(&location) {
+                return Some(keyword.content.clone());
+            }
+        }
+
+        None
     }
 }
 
@@ -61,8 +67,91 @@ pub struct LocationRange {
     pub end: LineColumn,
 }
 
+impl LocationRange {
+    fn contains(&self, lineColumn: &LineColumn) -> bool {
+        if self.start.line == self.end.line {
+            self.start.line == lineColumn.line
+                && self.start.column <= lineColumn.column
+                && lineColumn.column <= self.end.column
+        } else {
+            panic!()
+        }
+    }
+}
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct LineColumn {
     pub line: isize,
     pub column: isize,
+}
+
+#[test]
+fn test_get_hover_message() {
+    let file = File {
+        script: Script {
+            statements: vec![Statement::StmPrintStatement(PrintStatement {
+                print_keyword: {
+                    Keyword {
+                        content: "print".to_string(),
+                        location: LocationRange {
+                            start: LineColumn { line: 1, column: 0 },
+                            end: LineColumn { line: 1, column: 4 },
+                        },
+                    }
+                },
+                expression: Expression::ExpStringLiteral(StringLiteral {
+                    content: "Hello, World!".to_string(),
+                    location: LocationRange {
+                        start: LineColumn { line: 1, column: 6 },
+                        end: LineColumn {
+                            line: 1,
+                            column: 20,
+                        },
+                    },
+                }),
+                location: LocationRange {
+                    start: LineColumn { line: 1, column: 0 },
+                    end: LineColumn {
+                        line: 1,
+                        column: 20,
+                    },
+                },
+            })],
+            location: LocationRange {
+                start: LineColumn { line: 1, column: 0 },
+                end: LineColumn {
+                    line: 1,
+                    column: 20,
+                },
+            },
+        },
+        keywords_by_location: vec![(
+            LocationRange {
+                start: LineColumn { line: 1, column: 0 },
+                end: LineColumn { line: 1, column: 4 },
+            },
+            Keyword {
+                content: "print".to_string(),
+                location: LocationRange {
+                    start: LineColumn { line: 1, column: 0 },
+                    end: LineColumn { line: 1, column: 4 },
+                },
+            },
+        )],
+    };
+
+    assert_eq!(
+        file.get_hover_message(LineColumn { line: 1, column: 0 }),
+        Some("print".to_string())
+    );
+
+    assert_eq!(
+        file.get_hover_message(LineColumn { line: 1, column: 5 }),
+        None,
+    );
+
+    assert_eq!(
+        file.get_hover_message(LineColumn { line: 1, column: 6 }),
+        None,
+    );
 }
