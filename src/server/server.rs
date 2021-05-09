d46f652c051ffc0289ddaa48713705c8b7ede397
Christopher Wells <cwellsny@nycap.rr.com>
2021-05-08 22:57:35 -0700

    Implement message header parsing

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,22 +1,25 @@
 use std::io::{BufRead, Write};
+use std::str::from_utf8;
 use std::sync::{Arc, Mutex};
 
 use jsonrpc_core::{IoHandler, Params};
 use lsp_types::{DidOpenTextDocumentParams, HoverParams, InitializeParams, InitializeResult};
+use regex::Regex;
 use serde_json::{self, from_value, to_value, Value};
 
 use crate::server::state::ServerState;
 
-pub fn run<R, W>(input: R, mut output: W)
+pub fn run<R, W>(mut input: R, mut output: W)
 where
     R: BufRead,
     W: Write,
 {
     let io = setup_handler();
 
-    for line in input.lines() {
-        let line = line.unwrap();
-        let response = io.handle_request_sync(&line);
+    loop {
+        let message = Message::read_from_buffered_reader(&mut input);
+
+        let response = io.handle_request_sync(&message.content);
         if let Some(response) = response {
             writeln!(output, "{}", response).unwrap();
         }
@@ -66,5 +69,63 @@ fn params_to_value(params: Params) -> Value {
         Params::Map(map) => Value::Object(map),
         Params::Array(arr) => Value::Array(arr),
         Params::None => Value::Null,
+    }
+}
+
+#[derive(Debug)]
+struct Header {
+    content_length: u64,
+    content_type: Option<String>,
+}
+
+impl Header {
+    fn read_from_buffered_reader<R: BufRead>(input: &mut R) -> Header {
+        let mut content_length_line = String::new();
+        input.read_line(&mut content_length_line).unwrap();
+
+        let mut content_type_line = String::new();
+        input.read_line(&mut content_type_line).unwrap();
+
+        let content_length = Header::parse_content_length(&content_length_line);
+        let content_type = Header::parse_content_type(&content_type_line);
+
+        Header {
+            content_length,
+            content_type,
+        }
+    }
+
+    fn parse_content_length(line: &str) -> u64 {
+        let re = Regex::new(r"^Content-Length: (\d+)\r\n").unwrap();
+        let length_string = re.captures(line).unwrap()[1].to_string();
+
+        length_string.parse::<u64>().unwrap()
+    }
+
+    fn parse_content_type(line: &str) -> Option<String> {
+        if line == "\r\n" {
+            None
+        } else {
+            Some(line.to_string())
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Message {
+    header: Header,
+    content: String,
+}
+
+impl Message {
+    fn read_from_buffered_reader<R: BufRead>(input: &mut R) -> Message {
+        let header = Header::read_from_buffered_reader(input);
+
+        let mut content_buf: Vec<u8> = vec![0; header.content_length as usize];
+        input.read_exact(&mut content_buf).unwrap();
+
+        let content = from_utf8(&content_buf).unwrap().to_string();
+
+        Message { header, content }
     }
 }
